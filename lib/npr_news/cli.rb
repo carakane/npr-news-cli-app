e06fe7100a45c82a78e4325d01f12b90a6132f58
jessica fletcher <cara.kane@gmail.com>
2017-05-19 14:14:39 +0000

    partial interface stub

--- lib/npr_news/cli.rb
+++ lib/npr_news/cli.rb
@@ -1,7 +1,36 @@
 class NPRNews::CLI
 
   def call
+    welcome
+    divisions
+    division_choice
+  end
+
+  def welcome
     puts "Welcome to an unofficial NPR News Feed"
+    puts "All content in this feed is copyright NPR News"
+  end
+
+  def divisions
+    #iterates over the news divisions and puts them/or hard coded?/or scraped titles?
+    puts "Please choose one of the news divisions by number:"
+    puts "1. U.S."
+    puts "2. World"
+  end
+
+  def division_choice
+    input = gets.strip
+    if input == "1"
+      puts "US Headlines"
+    elsif input == "2"
+      puts "World Headlines"
+    elsif input == "exit"
+      puts "Goodbye!"
+    end
+  end
+
+  def headlines
+    #scrape headlines?
   end
 
 end
