e76e53c7918c0ec59dc61faaf08eb627cb3223fe
jessica fletcher <cara.kane@gmail.com>
2017-05-19 17:13:45 +0000

    stubbed data

--- lib/npr_news/news.rb
+++ lib/npr_news/news.rb
@@ -1,5 +1,23 @@
 class NPRNews::News
+  attr_accessor :title, :snippet, :url
+  # current thinking is that maybe divisions should be hard coded.
+  # def self.division
+  #   #should return division instances, divisions have names
+  #   division_1 = "U.S."
+  #   division_2 = "World"
+  # end
 
+  def self.headlines
+    #should return headlines, headlines also have snippets and URLs
+    headline_1 = self.new
+    headline_1.title = "Test Title"
+    headline_1.snippet = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    headline_1.url = "http://www.npr.org/sections/health-shots/2017/05/19/528516985/poll-doctors-are-still-prescribing-lots-of-opioids-for-low-back-pain"
 
+    headline_2 = self.new
+    headline_2.title = "2nd Test Title"
+    headline_2.snippet = "2nd Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    headline_2.url = "http://www.npr.org/sections/health-shots/2017/05/19/528516985/poll-doctors-are-still-prescribing-lots-of-opioids-for-low-back-pain"
+  end
 
 end
