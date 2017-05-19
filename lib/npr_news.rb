394fef0763707b1b05249729190b0232d7312103
jessica fletcher <cara.kane@gmail.com>
2017-05-19 19:18:42 +0000

    commented out unnecessary dependencies

--- lib/npr_news.rb
+++ lib/npr_news.rb
@@ -1,6 +1,11 @@
+require "open-uri"
 require "nokogiri"
 require "pry"
 
-require_relative "npr_news/version"
-require_relative "npr_news/cli.rb"
-require_relative "npr_news/news.rb"
+#require_relative "npr_news"
+require_relative "./npr_news/version"
+require_relative "./npr_news/cli.rb"
+require_relative "./npr_news/news.rb"
+#
+# module NPRNews
+# end
