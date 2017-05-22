469a9a305edafd1debfcc3dc623d1a44dd013183
jessica fletcher <cara.kane@gmail.com>
2017-05-22 13:55:38 +0000

    updated references in env

--- lib/npr_news.rb
+++ lib/npr_news.rb
@@ -5,7 +5,8 @@ require "pry"
 #require_relative "npr_news"
 require_relative "./npr_news/version"
 require_relative "./npr_news/cli.rb"
-require_relative "./npr_news/news.rb"
+require_relative "./npr_news/divisions.rb"
+require_relative "./npr_news/headlines.rb"
 #
 # module NPRNews
 # end
