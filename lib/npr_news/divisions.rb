a6e6e14c7c7e44b3341f239dee4e2591199a6c87
jessica fletcher <cara.kane@gmail.com>
2017-05-22 13:52:26 +0000

    renamed news.rb

---
+++ lib/npr_news/divisions.rb
@@ -0,0 +1,19 @@
+class NPRNews::Divisions
+  attr_accessor :name, :url
+
+  @@divisions = []
+
+  def self.division
+    #should return division instances, divisions have names
+    doc = Nokogiri::HTML(open("http://www.npr.org/sections/news/"))
+
+    doc.css("div.subtopics li").each do|div|
+      division = self.new
+      division.name = div.css("a").text
+      division.url = "http://www.npr.org#{div.css("a").attribute("href").value}"
+      @@divisions << division
+    end
+    @@divisions
+  end
+
+end
