0a44c78d419a64fb79f2f7427dae6b70433fa894
jessica fletcher <cara.kane@gmail.com>
2017-05-22 14:01:08 +0000

    fixed scrape

--- lib/npr_news/headlines.rb
+++ lib/npr_news/headlines.rb
@@ -10,12 +10,12 @@ class NPRNews::Headlines
   def self.headline_scrape(division_url)
     doc = Nokogiri::HTML(open(division_url))
 
-    doc.css("article.item.has-image").each do |article| #div.featured # article.item.has-image
+    doc.css("article.item.has-image").each do |article|
       headline = self.new
-      headline.title = "test",#article.css("div.item-info h2 a").text,
-      headline.url = "test1",#article.css("div.item-info h2 a").attribute("href").value,
-      headline.snippet = "test2"#article.css("div.item-info p.teaser a").text.gsub(/(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)\s+\d{1,2},\s+\d{4}\s../,"") #.{7}
-      binding.pry
+      headline.title = article.css("div.item-info h2 a").text
+      headline.url = article.css("div.item-info h2 a").attribute("href").value
+      headline.snippet = article.css("div.item-info p.teaser a").text.gsub(/(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)\s+\d{1,2},\s+\d{4}\s../,"") #.{7}
+      #binding.pry
       @@headlines << headline
     end
     @@headlines
